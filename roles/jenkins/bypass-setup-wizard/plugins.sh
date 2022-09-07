6b719750bf679353f6f18164db4e5604c911fd2e
saiful@regul <saiful@jogjacamp.com>
2022-09-07 11:10:37 +0700

    Comit pertama

---
+++ roles/jenkins/bypass-setup-wizard/plugins.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+declare -a PluginList=(
+    "gitea" 
+    "ant"
+    "antisamy-markup-formatter"
+    "build-timeout"
+    "cloudbees-folder"
+    "configuration-as-code"
+    "credentials-binding"
+    "email-ext"
+    "git"
+    "github-branch-source"
+    "gradle"
+    "ldap"
+    "mailer"
+    "matrix-auth"
+    "pam-auth"
+    "pipeline-github-lib"
+    "pipeline-stage-view"
+    "ssh-slaves"
+    "timestamper"
+    "workflow-aggregator"
+    "ws-cleanup"
+    "docker-plugin"
+)
+
+for plugin in ${PluginList[@]}; do
+   java -jar jenkins-cli.jar -auth admin:admin -s http://localhost:8080/ install-plugin $plugin
+done
